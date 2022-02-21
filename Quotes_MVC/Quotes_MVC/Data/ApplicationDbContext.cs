f2a00e7f809698b3f1ccfc625d7a66227ef96e4b
Karanbir Singh <karanbir_singh@dialog.com.au>
2022-02-21 12:04:27 +1030

    create controller, add migration, created table

--- Quotes_MVC/Quotes_MVC/Data/ApplicationDbContext.cs
+++ Quotes_MVC/Quotes_MVC/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Quotes_MVC.Models;
 
 namespace Quotes_MVC.Data
 {
@@ -12,5 +13,6 @@ namespace Quotes_MVC.Data
             : base(options)
         {
         }
+        public DbSet<Quotes_MVC.Models.Quotes> Quotes { get; set; }
     }
 }
---
+++ Quotes_MVC/Quotes_MVC/Data/Migrations/20220221012657_InitialSetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quotes_MVC.Data.Migrations
+{
+    public partial class InitialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuoteAuther = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QuoteText = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+        }
+    }
+}

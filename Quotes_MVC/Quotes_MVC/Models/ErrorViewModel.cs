854af1e3d7309061fc54cd84da8965ded8bb78bc
Karanbir Singh <karanbir_singh@dialog.com.au>
2022-02-21 11:26:45 +1030

    intiated the Quotes project and models i.e id, auther and text

---
+++ Quotes_MVC/Quotes_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Quotes_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Quotes_MVC/Quotes_MVC/Models/Quotes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quotes_MVC.Models
+{
+    public class Quotes
+    {
+        public int Id { get; set; }
+        public string QuoteAuther { get; set; }
+        public string QuoteText { get; set; }
+
+        public Quotes()
+        {
+
+        }
+    }
+}
